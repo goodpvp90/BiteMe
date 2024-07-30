d593ee032205e0265ebd9267ecba92e220b929be
nadir278 <nadir34024@gmail.com>
2024-07-30 11:59:49 +0300

    fix the fix

--- BiteMeClient/src/client/Client.java
+++ BiteMeClient/src/client/Client.java
@@ -85,7 +85,6 @@ public class Client extends AbstractClient {
 			case USER:
 				//SEND TO CLIENT CONTROLLER
 	        	Object[] data = (Object[])message[1];
-				System.out.println((boolean)data[1]);
 	        	clientLoginController.updateUser(data);
 	        	break;
 			case INSERT_ORDER:
