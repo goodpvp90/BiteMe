1e1e3dcab566439c407f7cf5aef202988a4cf849
nadir278 <nadir34024@gmail.com>
2024-08-06 19:36:54 +0300

    changed all functinality of perfomance report little changes for order object could be changed or deleted depends on what u want

--- BiteMeSrv/src/common/PerformanceReport.java
+++ BiteMeSrv/src/common/PerformanceReport.java
@@ -1,28 +1,25 @@
 package common;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
 import common.Restaurant.Location;
 
-public class PerformanceReport extends MonthlyReport{
-    private int totalOrders;
-    private int ordersCompletedInTime;
+public class PerformanceReport extends MonthlyReport {
+    
+    private List<DailyPerformanceReport> dailyReports;
 
     public PerformanceReport(Location restaurant, int month, int year) {
-		super(restaurant, month, year);
-	}
-
-    public int getTotalOrders() {
-        return totalOrders;
+        super(restaurant, month, year);
+        this.dailyReports = new ArrayList<>();
     }
 
-    public void setTotalOrders(int totalOrders) {
-        this.totalOrders = totalOrders;
+    public List<DailyPerformanceReport> getDailyReports() {
+        return dailyReports;
     }
 
-    public int getOrdersCompletedInTime() {
-        return ordersCompletedInTime;
-    }
-
-    public void setOrdersCompletedInTime(int ordersCompletedInTime) {
-        this.ordersCompletedInTime = ordersCompletedInTime;
+    public void addDailyReport(DailyPerformanceReport dailyReport) {
+        this.dailyReports.add(dailyReport);
     }
 }
