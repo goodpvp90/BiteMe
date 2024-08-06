96d5a4c0a9286a9d3e5a32bdc3249209e9383711
EldarGafarov <93073346+EldarGafarov@users.noreply.github.com>
2024-08-07 00:00:48 +0300

    Update to PerformanceReport now graphically it looks good

--- BiteMeClient/src/ClientGUI/PerformanceReportController.java
+++ BiteMeClient/src/ClientGUI/PerformanceReportController.java
@@ -2,20 +2,28 @@ package ClientGUI;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.chart.PieChart;
+import javafx.scene.chart.StackedBarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 import common.PerformanceReport;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import common.DailyPerformanceReport;
+import javafx.application.Platform;
+import javafx.scene.control.Tooltip;
+import javafx.scene.Node;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PerformanceReportController {
 
     @FXML private Label branchLabel;
     @FXML private Label monthYearLabel;
     @FXML private Label performancePercentageLabel;
-    @FXML private PieChart performanceChart;
-    @FXML private Label completedLabel;
-    @FXML private Label notCompletedLabel;
+    @FXML private StackedBarChart<String, Number> performanceChart;
+    @FXML private CategoryAxis xAxis;
+    @FXML private NumberAxis yAxis;
 
     private ReportsPageController reportsPageController;
 
@@ -25,30 +33,62 @@ public class PerformanceReportController {
 
     public void setReportData(PerformanceReport report) {
         branchLabel.setText("Branch: " + report.getRestaurant().toString());
-        monthYearLabel.setText("Produced at: " + report.getMonth() + "/" + report.getYear());
+        monthYearLabel.setText("Report for: " + report.getMonth() + "/" + report.getYear());
         
-        int totalOrders = report.getTotalOrders();
-        int ordersCompletedInTime = report.getOrdersCompletedInTime();
-        int ordersNotCompletedInTime = totalOrders - ordersCompletedInTime;
-        
-        double performancePercentage = (double) ordersCompletedInTime / totalOrders * 100;
-        performancePercentageLabel.setText(String.format("Performance: %.2f%%", performancePercentage));
+        xAxis.setLabel("Day of Month");
+        yAxis.setLabel("Number of Orders");
 
-        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
-            new PieChart.Data("", ordersCompletedInTime),
-            new PieChart.Data("", ordersNotCompletedInTime)
-        );
-        
-        performanceChart.setData(pieChartData);
-        performanceChart.setStartAngle(90);
+        XYChart.Series<String, Number> completedSeries = new XYChart.Series<>();
+        completedSeries.setName("Completed in Time");
 
-        pieChartData.get(0).getNode().setStyle("-fx-pie-color: #2ecc71;");
-        pieChartData.get(1).getNode().setStyle("-fx-pie-color: #e74c3c;");
-        
-        performanceChart.setLegendVisible(false);
-        
-        completedLabel.setText(String.format("Completed in Time: %.1f (%.1f%%)", (double)ordersCompletedInTime, performancePercentage));
-        notCompletedLabel.setText(String.format("Not Completed in Time: %.1f (%.1f%%)", (double)ordersNotCompletedInTime, 100 - performancePercentage));
+        XYChart.Series<String, Number> notCompletedSeries = new XYChart.Series<>();
+        notCompletedSeries.setName("Not Completed in Time");
+
+        List<DailyPerformanceReport> dailyReports = report.getDailyReports();
+        int totalOrdersMonth = 0;
+        int completedOrdersMonth = 0;
+
+        for (DailyPerformanceReport dailyReport : dailyReports) {
+            String day = String.valueOf(dailyReport.getDate().getDate());
+            int totalOrders = dailyReport.getTotalOrders();
+            int completedOrders = dailyReport.getOrdersCompletedInTime();
+            int notCompletedOrders = totalOrders - completedOrders;
+
+            completedSeries.getData().add(new XYChart.Data<>(day, completedOrders));
+            notCompletedSeries.getData().add(new XYChart.Data<>(day, notCompletedOrders));
+
+            totalOrdersMonth += totalOrders;
+            completedOrdersMonth += completedOrders;
+        }
+
+        performanceChart.getData().addAll(completedSeries, notCompletedSeries);
+
+        // Calculate overall performance percentage
+        double performancePercentage = (double) completedOrdersMonth / totalOrdersMonth * 100;
+        performancePercentageLabel.setText(String.format("Overall Performance: %.2f%%", performancePercentage));
+
+        // Apply tooltips after chart has been laid out
+        Platform.runLater(this::applyTooltips);
+    }
+
+    private void applyTooltips() {
+        for (XYChart.Series<String, Number> series : performanceChart.getData()) {
+            for (XYChart.Data<String, Number> data : series.getData()) {
+                Node node = data.getNode();
+                if (node != null) {
+                    String day = data.getXValue();
+                    int value = data.getYValue().intValue();
+                    String seriesName = series.getName();
+                    
+                    Tooltip tooltip = new Tooltip(String.format("%s on Day %s: %d orders", seriesName, day, value));
+                    tooltip.setStyle("-fx-font-size: 14px; -fx-padding: 5px;");
+                    Tooltip.install(node, tooltip);
+
+                    node.setOnMouseEntered(event -> node.setOpacity(0.8));
+                    node.setOnMouseExited(event -> node.setOpacity(1));
+                }
+            }
+        }
     }
 
     @FXML
